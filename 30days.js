8fb99aba3039859fd23fe401e75907033461658e
Solom0m <122858602+Solom0m@users.noreply.github.com>
2024-05-10 15:18:55 +0100

    Add files via upload

---
+++ 30days.js
@@ -0,0 +1,40 @@
+// Comments can make code readable
+// Welcome to 30days of javascript
+
+/* Comments can make code readable
+  easy to reuse and informative
+  */
+
+  let firstName = "Solomon";
+  let boolean = true;
+  let man = undefined
+  let data = null
+  console.log(firstName, boolean , undefined , data)
+
+  let boy;
+  let woman;
+  let kids;
+  let men;
+
+  let me = "Chinonso";
+  let ada = "girl";
+  let amount = 90;
+  let market = true;
+  console.log(me, ada, amount, market)
+
+  /*let firstNam = 'Chinonso'
+  let LastName = 'Nnamani'
+  let maritalStatus = 'single'
+  let country = 'Nigeria'
+  let age = 21
+
+  console.log(firstNam, LastName, maritalStatus, country, age)*/
+
+  let firstNam = 'Chinonso', LastName = 'Nnamani', maritalStatus = 'single', country = 'Nigeria', age = 21
+  console.log(firstNam, LastName, maritalStatus, country, age)
+
+  let myAge = 25
+  let yourAge = 30
+
+  console.log(' i am ' + myAge + " years old")
+  console.log("You are " + 30 + " years old")
---
+++ 30dayz.js
@@ -0,0 +1,5 @@
+console.log(typeof "Solomon");
+console.log(typeof true);
+console.log(typeof 78);
+console.log(typeof undefined);
+console.log(typeof null);
