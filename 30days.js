e3678daab432dea981e56589151cd31fa09d8afc
Solom0m <solomonnnamani01@gmail.com>
2024-05-13 18:27:10 +0100

    2nd daay

--- 30days.js
+++ 30days.js
@@ -1,11 +1,13 @@
+/* 1st day
+
 // Comments can make code readable
 // Welcome to 30days of javascript
 
-/* Comments can make code readable
+ Comments can make code readable
   easy to reuse and informative
   */
 
-  let firstName = "Solomon";
+  /*let firstName = "Solomon";
   let boolean = true;
   let man = undefined
   let data = null
@@ -22,13 +24,13 @@
   let market = true;
   console.log(me, ada, amount, market)
 
-  /*let firstNam = 'Chinonso'
-  let LastName = 'Nnamani'
-  let maritalStatus = 'single'
-  let country = 'Nigeria'
-  let age = 21
+  //let firstNam = 'Chinonso'
+  //let LastName = 'Nnamani'
+  //let maritalStatus = 'single'
+  //let country = 'Nigeria'
+  //let age = 21
 
-  console.log(firstNam, LastName, maritalStatus, country, age)*/
+  //console.log(firstNam, LastName, maritalStatus, country, age)
 
   let firstNam = 'Chinonso', LastName = 'Nnamani', maritalStatus = 'single', country = 'Nigeria', age = 21
   console.log(firstNam, LastName, maritalStatus, country, age)
@@ -37,4 +39,135 @@
   let yourAge = 30
 
   console.log(' i am ' + myAge + " years old")
-  console.log("You are " + 30 + " years old")
+  console.log("You are " + 30 + " years old") 
+  
+  END*/
+
+
+
+/* 2nd day
+
+  let challenge = '30 Days Of JavaScript'
+
+  //console.log(challenge.length)
+  //cconsole.log(challenge.toUpperCase())
+  // console.log(challenge.toLowerCase())
+    //  console.log(challenge.substr(3,20))
+   // console.log(challenge.substring(2,21))
+    //console.log(challenge.substr(0,2))
+    //console.log(challenge.includes("Script"))
+    //console.log(challenge.split())
+    //console.log(challenge.split(' '))
+    let companies = 'Facebook, Google,Microsoft,Apple,IBM,Oracle,Amazon'
+    const arr = companies.split(',')
+    console.log(arr)
+
+   // console.log(challenge.replace("JavaScript", "Python"))
+  // console.log(challenge.charAt(15))
+ // console.log(challenge.charCodeAt(11))
+   // console.log(challenge.indexOf("a"))
+    //console.log(challenge.lastIndexOf("a"))
+    let text = 'You cannot end a sentence with because because because is a conjunction'
+    //console.log(text.lastIndexOf("because"))
+    //console.log(text.search(/because/gi))
+    //console.log(text.trim())
+    //console.log(challenge.startsWith(30))
+    //console.log(challenge.endsWith("JavaScript"))
+    //console.log(challenge.match(/a/gi))
+  //console.log(challenge.concat("JavaScript"))
+  //console.log(challenge.repeat(2))
+  
+
+  
+  
+   // console.log('There is no exercise better for the heart than reaching down and lifting \
+//people up');
+//console.log("Love is not patronizing and charity isnt about pity, it is about love")
+
+ // let num = "10"
+ // let numBer = +num
+  //console.log(typeof numBer)
+
+ 
+   // let value = parseFloat('9.8')
+   // value = Math.round(value)
+   // console.log(value)
+
+   // let a = "python jargon"
+   // console.log(a.match("on"))
+
+    //let b = " i hope this course is not full of jargon"
+    //console.log(b.includes("jargon"))
+
+   // let a = Math.floor(Math.random() * 101)
+   // console.log(a)
+
+   //let a = Math.floor(Math.random() * 51)+50
+   // console.log(a)
+
+  // let a = Math.floor(Math.random() * 256)
+   // console.log(a)
+
+  // let a = Math.floor(Math.random()*10);
+  //let b = "JavaScript"[a]
+  //console.log(b)
+
+  /*console.log("1\t1\t1\t1\t1");
+  console.log("2\t1\t2\t4\t8");
+  console.log("3\t1\t3\t9\t27");
+  console.log("4\t1\t4\t16\t64");
+  console.log("5\t1\t5\t25\t125");*/
+
+  //let a = "You cannot end a setence with because because because is a conjunction"
+  //console.log(a.substr(30, 23))
+
+  //'love is the best thing in the world 
+  //Some found their love and some are still looking for their love'
+  // count the number of the word love in the sentence
+
+    //let a = 'love is the best thing in the world.\
+   // Some found their love and some are still looking for their love'
+    //let b = a.match(/love/gi)
+   // console.log(b.length)
+
+   //let a = 'You cannot end a setence with because because because is a conjunction'
+  // let b = a.match(/because/gi).length
+  // console.log(b)
+
+  //let sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
+  //let b = sentence.replace('%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching', 
+   // 'I am a teacher, and i love teaching. There is nothing as more rewarding as educating and empowering people. I found teaching more interesting than any other jobs. Does this motivate you to be a teacher? This 30Days Of Javascript is also the result of love of teaching.').match(/teaching/gi)
+   // console.log(b)
+
+   //Calculate the total annual income of the person by extracting 
+   //the numbers from the following text. 'He earns 5000 euro from 
+   //salary per month, 10000 euro annual bonus, 15000 euro online courses 
+   //per month.'
+
+    // let earn = 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'
+  //let b = earn.match(/\d+/g)
+  //console.log(parseInt(b[0])+parseInt(b[1])+parseInt(b[2]))  */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
--- 30dayz.js
+++ 30dayz.js
@@ -1,5 +1,7 @@
+/* 1ST DAY
+
 console.log(typeof "Solomon");
 console.log(typeof true);
 console.log(typeof 78);
 console.log(typeof undefined);
-console.log(typeof null);
+console.log(typeof null); */
